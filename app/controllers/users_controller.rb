79fb1f9142e05fefe2d13889ff145296f0754f9f
Wendy Gwo <wgwo@hotmail.com>
2015-02-09 09:27:44 -0800

    Add edit/update funcationality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
-	
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -17,6 +17,17 @@ class UsersController < ApplicationController
 			redirect_to users_path
 		else
 			render 'new'
+		end
+	end
+	def edit
+		@user = User.find(params[:id])
+	end
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params.require(:user).permit(:username, :email, :dob))
+			redirect_to users_path
+		else
+			render 'edit'
 		end
 	end
 
